00f17c5d5b757ddece47e08e55495d5e51d7b427
Ilian Ivanov <ilian_oven@abv.bg>
2013-08-14 15:35:14 +0300

    Added Albums Controller

---
+++ ImageGallery/ImageGalery.Services/Controllers/AlbumsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using ImageGallery.Models;
+using ImageGallery.DataLayer;
+
+namespace ImageGalery.Services.Controllers
+{
+    public class AlbumsController : ApiController
+    {
+        private ImageGalleryContext db = new ImageGalleryContext();
+
+        // GET api/Albums
+        public IEnumerable<Album> GetAlbums()
+        {
+            return db.Albums.AsEnumerable();
+        }
+
+        // GET api/Albums/5
+        public Album GetAlbum(int id)
+        {
+            Album album = db.Albums.Find(id);
+            if (album == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return album;
+        }
+
+        // PUT api/Albums/5
+        public HttpResponseMessage PutAlbum(int id, Album album)
+        {
+            if (ModelState.IsValid && id == album.AlbumId)
+            {
+                db.Entry(album).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // POST api/Albums
+        public HttpResponseMessage PostAlbum(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Albums.Add(album);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, album);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = album.AlbumId }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // DELETE api/Albums/5
+        public HttpResponseMessage DeleteAlbum(int id)
+        {
+            Album album = db.Albums.Find(id);
+            if (album == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Albums.Remove(album);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, album);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
