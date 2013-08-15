25dd32ff40203906ef04f812f53364833f2b730e
Pavel Sotirov <me@psotirov.cloudvps.bg>
2013-08-15 22:10:49 +0300

    Only web services to appharbour

---
+++ ImageGallery/ImageGalery.Services/Controllers/CommentsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using ImageGallery.Models;
+using ImageGallery.DataLayer;
+
+namespace ImageGalery.Services.Controllers
+{
+    public class CommentsController : ApiController
+    {
+        private ImageGalleryContext db = new ImageGalleryContext();
+        public CommentsController()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+       
+
+        // GET api/Comments/5
+        public HttpResponseMessage GetComment(int id)
+        {
+            var image = db.Images.FirstOrDefault(x => x.ImageId == id);
+
+            var comments = db.Comments.Where(x => x.ImageId == id).Select(y=> new {
+                CommentId=y.CommentId,
+                Content=y.Content,
+                Username=y.User.Username,
+                ImageId = y.ImageId
+            }).ToList();
+
+            if (image == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, comments);
+        }
+
+        // PUT api/Comments/5
+        public HttpResponseMessage PutComment(int id, Comment comment)
+        {
+            //Check isLoggedIn
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != comment.CommentId)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(comment).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/Comments
+        public HttpResponseMessage PostComment(Comment comment)
+        {
+            //Check isLoggedIn
+            if (ModelState.IsValid)
+            {
+                db.Comments.Add(comment);
+                db.SaveChanges();
+               
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, comment);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = comment.CommentId }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/Comments/5
+        public HttpResponseMessage DeleteComment(int id)
+        {
+            //Check isLoggedIn
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Comments.Remove(comment);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, comment);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ImageGallery/ImageGalery.Services/Controllers/ImagesController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using ImageGallery.Models;
+using ImageGallery.DataLayer;
+using ImageGallery.Models;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using ImageGalery.Services.Persisters;
+namespace ImageGalery.Services.Controllers
+{
+    public class ImagesController : ApiController
+    {
+        private ImageGalleryContext db = new ImageGalleryContext();
+
+        public ImagesController()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+
+        // GET api/Images
+        public IEnumerable<Image> GetImages()
+        {
+            return db.Images.AsEnumerable();
+        }
+
+        public HttpResponseMessage GetImage(int id)
+        {
+            var comments = (from comment in db.Comments
+                            where (comment.Image.ImageId == id)
+                            select new
+                            {
+                                Content = comment.Content,
+                                Username = comment.User.Username
+                            }).ToList();
+            var image = db.Images.Find(id);
+
+            if (image != null)
+            {
+                var data = new
+                {
+                    ImageId = image.ImageId,
+                    Title = image.Title,
+                    Comments = comments,
+                    Url = image.Url
+                };
+
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        // POST api/Images
+        // POST api/Images
+        public async Task<HttpResponseMessage> PostImage(string title, int userId, int albumId)
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+            //string debug = "2";
+            string root = HttpContext.Current.Server.MapPath("~/App_Data");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                // Read the form data.
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                // This illustrates how to get the file names.
+                foreach (MultipartFileData file in provider.FileData)
+                {
+                    Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                    Trace.WriteLine("Server file path: " + file.LocalFileName);
+                    string fileName = file.LocalFileName;
+                    string hardcodedName = "test-image.jpg";
+
+                    var url = DropBoxUploader.UploadProfilePicToDropBox(file.LocalFileName, file.Headers.ContentDisposition.FileName);
+
+                    db.Images.Add(new Image
+                    {
+                        Title = title,
+                        Url = url,
+                        Album = db.Albums.FirstOrDefault(x => x.AlbumId == albumId)
+                    });
+                    db.SaveChanges();
+                    break;
+                }
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (System.Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
